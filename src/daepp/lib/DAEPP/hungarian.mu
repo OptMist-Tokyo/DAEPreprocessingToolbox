a8b200c942053933899fdfa8274681446b70f9d6
Taihei OKI <t.oki.natrium@gmail.com>
2019-04-28 19:08:50 +0900

    Make hungarian.m private

--- src/daepp/lib/DAEPP/hungarian.mu
+++ src/daepp/lib/DAEPP/hungarian.mu
@@ -1,4 +1,22 @@
-// MuPAD implementation for hungarian.m
+/*
+  Compute a maximum assignment for an n x n cost matrix M by the Hungarian
+  method.
+  Also return an optimal solution (p1, ..., pn, q1, ..., qn) of the
+  following dual problem:
+
+      minimize    (q_1 + ... + q_n) - (p_1 + ... + p_n)
+      subject to  q_j - p_i >= M(i, j)                    (1 <= i, j <= n)
+                  q_j and p_i are nonnegative integers    (1 <= i, j <= n)
+
+  Return Values:
+      - optval : the optimal value. If M has no perfect matching, this is
+                 set to -Inf.
+      -      s : the i-th row is assigned to the s(i)-th column
+      -      t : the j-th column is assigned to the t(j)-th row
+                 (t is the inverse of s as a permutation)
+      -      p : optimal dual solution in rows
+      -      q : optimal dual solution in columns
+*/
 
 daepp::hungarian := proc(M)
 local m, n, optval, s, t, p, q, updateSlack, r, slack, slackid, prev,
--- test/hungarianTest.m
+++ test/hungarianTest.m
@@ -1,8 +1,25 @@
 classdef hungarianTest < matlab.unittest.TestCase
     methods (Static)
+        function [optval, s, t, p, q] = hungarian(M)
+            % call MuPAD
+            loadMuPADPackage;
+            try
+                out = feval(symengine, 'daepp::hungarian', M);
+            catch ME
+                throw(ME);
+            end
+            
+            % restore return values
+            optval = double(out(1));
+            s = double(out(2));
+            t = double(out(3));
+            p = double(out(4));
+            q = double(out(5));
+        end
+        
         function tf = check(M)
             [n, ~] = size(M);
-            [v, s, t, p, q] = hungarian(M);
+            [v, s, t, p, q] = hungarianTest.hungarian(M);
             
             % check if s and t are permutations on {1,....,n}
             if any(sort(s) ~= 1:n) || any(sort(t) ~= 1:n)
@@ -86,13 +103,13 @@ classdef hungarianTest < matlab.unittest.TestCase
 
         function test6(testCase)
             M = [-Inf 1 -Inf -Inf; -Inf -Inf 1 -Inf; -Inf -Inf -Inf 1; -Inf -Inf -Inf -Inf];
-            v = hungarian(M);
+            v = hungarianTest.hungarian(M);
             testCase.verifyEqual(v, -Inf);
         end
 
         function test7(testCase)
             M = [0 -Inf -Inf; 1 -Inf -Inf; 2 3 4];
-            v = hungarian(M);
+            v = hungarianTest.hungarian(M);
             testCase.verifyEqual(v, -Inf);
         end
         
